ef6ecee4a2f63988dec474c4d0fd8928ac36457e
LH <yuri924@163.com>
2015-06-04 12:50:56 +0800

    a little modify

--- app/src/main/java/com/example/l/courierapp/MainActivity.java
+++ app/src/main/java/com/example/l/courierapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.l.courierapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -18,20 +19,25 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ListView mlistview;
-    private static final String[] s = {"one", "two", "three", "four", "five","six","seven"};
-
+    private static final String[] orderid = {"1506030000", "1506030001", "1506030002", "1506030003", "1506030004","1506030005","1506030006"};
+    private static final String[] orderaddress = {"二期望海路55号","观日路44号厦门信息投资","软件园二期18号"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +45,32 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mlistview = (ListView)findViewById(R.id.listView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,s);
-        mlistview.setAdapter(arrayAdapter);
+
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+
+        for(int i=0; i<3; i++){
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemTitle", "订单号："+orderid[i]);
+            map.put("ItemText", "订单地址："+orderaddress[i]);
+            listItem.add(map);
+        }
+
+    SimpleAdapter simpleAdapter = new SimpleAdapter(this, listItem,R.layout.listviewitem, new String[]{"ItemTitle", "ItemText"},
+                    new int[] {R.id.ItemTitle,R.id.ItemText});
+
+
+
+    mlistview.setAdapter(simpleAdapter);
+
+    mlistview.setOnItemClickListener(new ListView.OnItemClickListener() {
+
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+            Toast.makeText(getApplicationContext(), "你选择了第" + arg2 + "行", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), OrderInfoActivity.class);
+            startActivity(intent);
+        }});
 
 
     }
--- app/src/main/java/com/example/l/courierapp/OrderInfoActivity.java
+++ app/src/main/java/com/example/l/courierapp/OrderInfoActivity.java
@@ -1,38 +1,69 @@
 package com.example.l.courierapp;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMap.OnMapClickListener;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 
-public class OrderInfoActivity extends ActionBarActivity {
+
+public class OrderInfoActivity extends Activity {
+
+    private MapView mapView;
+    private AMap aMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_info);
+        mapView = (MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        aMap = mapView.getMap();
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_order_info, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
+
+
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 }
