2318d7e673fabf9836564282ea19c4b28b735bce
LH <yuri924@163.com>
2015-06-05 12:33:03 +0800

    高德地图加入定位图层

--- app/src/main/java/com/example/l/courierapp/OrderInfoActivity.java
+++ app/src/main/java/com/example/l/courierapp/OrderInfoActivity.java
@@ -1,20 +1,29 @@
 package com.example.l.courierapp;
 
 import android.app.Activity;
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
-import com.example.l.courierapp.R;
 
-public class OrderInfoActivity extends Activity {
+public class OrderInfoActivity extends Activity implements LocationSource,
+        AMapLocationListener {
 
     private MapView mapView;
     private AMap aMap;
-
+    private LocationSource.OnLocationChangedListener mListener;
+    private LocationManagerProxy mAMapLocationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +31,16 @@ public class OrderInfoActivity extends Activity {
         mapView = (MapView)findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         aMap = mapView.getMap();
+        setUpMap();
 
+    }
+
+    private void setUpMap() {
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(false);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
 
     }
 
@@ -53,6 +71,64 @@ public class OrderInfoActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getAMapException().getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                Log.e("AmapErr", "Location ERR:" + amapLocation.getAMapException().getErrorCode());
+            }
+        }
+    }
+
+    /**
+     * 激活定位
+     */
+    @Override
+    public void activate(LocationSource.OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mAMapLocationManager == null) {
+            mAMapLocationManager = LocationManagerProxy.getInstance(this);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用destroy()方法
+            // 其中如果间隔时间为-1，则定位只定一次
+            // 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
+            mAMapLocationManager.requestLocationData(
+                    LocationProviderProxy.AMapNetwork, 60 * 1000, 10, this);
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mAMapLocationManager != null) {
+            mAMapLocationManager.removeUpdates(this);
+            mAMapLocationManager.destroy();
+        }
+        mAMapLocationManager = null;
     }
 
 }
